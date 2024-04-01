823a30866081a5f899ca59d6fece746f2f0db32e
Joana Akimana <joanarwem2@gmail.com>
2024-04-01 18:32:20 +0400

    work continues

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    workboxPluginMode: "GenerateSW",
+    skipWaiting: true,
+    workboxOptions: {
+      runtimeCaching: [{
+        // Routing via a matchCallback function:
+        urlPattern: ({request}) =>  request.destination === "image",
+        handler: 'StaleWhileRevalidate'
+      },
+        {
+          urlPattern: ({  url }) => url.origin === 'http://localhost:3000' && url.pathname === '/collection/lessons' ,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'api-cache'
+          }
+        }
+      ],
+    }}
 })
